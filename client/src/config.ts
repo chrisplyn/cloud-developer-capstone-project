9d060cb74d821e2d2d45a1831a97c6e3e6f59c03
yliu996 <yliu996@github.gatech.edu>
2020-07-16 23:44:04 -0400

    fix frontend api

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,6 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = process.env.API_ID
-const stage = process.env.STAGE
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/${stage}`
+const apiId = 'yf7pv6gehi'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/prod`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
